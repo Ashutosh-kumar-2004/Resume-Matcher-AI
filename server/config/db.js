3615bf0f031f26571971fe8da9abec8709022741
Ashutosh <ashutoshyadav202004@gmail.com>
2026-03-30 01:33:20 +0530

    db connected

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI + 'resumeMatcher AI');
+    const conn = await mongoose.connect(process.env.MONGODB_URI + 'resumeMatcherAI');
     console.log(`MongoDB Connected`);
     return conn;
   } catch (error) {
